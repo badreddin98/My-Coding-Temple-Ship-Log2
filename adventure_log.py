9caa9b1e7f073ef43e21069d30c4ab1b3cd82516
badreddin <badreddin@badreddins-MacBook-Pro.local>
2024-06-30 20:07:48 -0400

    updated python file

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-  print("ahoy, Coding Temple navigators! ready for today's adventure?")
+  print("Hello, Coding Temple navigators?")
 
   if __name__ == "__main__":
     greet_coding_temple()
